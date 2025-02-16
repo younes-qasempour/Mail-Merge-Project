27c1b97f644dc6f092beec39ccf68c63898fd9ec
younes_qasempour <qasempouryounes@gmail.com>
2025-02-16 22:16:45 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+with open("./Input/Letters/starting_letter.txt") as f:
+    letter_content = f.readlines()
+
+with open("./Input/Names/invited_names.txt") as f:
+    names_content = f.readlines()
+
+names = []
+for name in names_content:
+    names.append(name.strip())
+
+letter = ""
+for line in letter_content:
+    letter = letter + line
+
+for name in names:
+    with open(f"./Output/ReadyToSend/{name}letter.txt", "w") as f:
+        f.write(letter.replace("[name]", name))
